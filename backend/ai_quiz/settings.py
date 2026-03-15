9922f762a576242685f7a57e70b224c9b4ec3b6f
MadanikaSR <srmadanika2021@gmail.com>
2026-03-15 17:08:53 +0530

    Add production deployment files and scripts

--- backend/ai_quiz/settings.py
+++ backend/ai_quiz/settings.py
@@ -23,12 +23,14 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-50ib@=4y_8vzn*zqmtsl*)^2%b(p7q3&=i3oerwr#g8+pp6d81'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-50ib@=4y_8vzn*zqmtsl*)^2%b(p7q3&=i3oerwr#g8+pp6d81')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+if ALLOWED_HOSTS == ['']:
+    ALLOWED_HOSTS = ['*'] # Fallback for development
 
 
 # Application definition
@@ -48,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -80,11 +83,13 @@ WSGI_APPLICATION = 'ai_quiz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 
@@ -123,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import os
 from datetime import timedelta
